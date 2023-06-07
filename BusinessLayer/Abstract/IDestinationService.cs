7eac450f83898774d3d2bff357d8a4626454bd87
Ahmet Özdemir <ahmetzdmiir@gmail.com>
2023-06-07 04:58:57 +0300

    FluentValidation kurulumu yapıldı

---
+++ BusinessLayer/Abstract/IDestinationService.cs
@@ -0,0 +1,14 @@
+﻿using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EntityLayer.Concrete.Destination;
+
+namespace BusinessLayer.Abstract
+{
+  public  interface IDestinationService : IGenericService<Destination>
+    {
+    }
+}
