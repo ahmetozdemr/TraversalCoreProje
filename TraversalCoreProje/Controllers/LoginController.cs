707019a9baa36a310593f69934508acd2b0f6948
Ahmet Özdemir <ahmetzdmiir@gmail.com>
2023-06-13 14:45:24 +0300

    34 Identity ile Login

--- TraversalCoreProje/Controllers/LoginController.cs
+++ TraversalCoreProje/Controllers/LoginController.cs
@@ -10,10 +10,12 @@ namespace TraversalCoreProje.Controllers
     public class LoginController : Controller
     {
         private readonly UserManager<AppUser> _userManager;
+        private readonly SignInManager<AppUser> _signInManager;
 
-        public LoginController(UserManager<AppUser> userManager)
+        public LoginController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager)
         {
             _userManager = userManager;
+            _signInManager = signInManager;
         }
 
         [HttpGet]
@@ -57,6 +59,24 @@ namespace TraversalCoreProje.Controllers
         [HttpGet]
         public IActionResult SignIn()
         {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> SignIn(UserSignInViewModel p)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await _signInManager.PasswordSignInAsync(p.username, p.password, false, true);
+
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index", "Destination");
+                }
+                else
+                {
+                    return RedirectToAction("SignIn", "Login");
+                }
+            }
             return View();
         }
     }
